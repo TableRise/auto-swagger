a848f92e35ce13cd793ad6c2e91dae360cbd429d
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-06-24 12:45:03 -0300

    ref: Remove comments

--- index.js
+++ index.js
@@ -3,28 +3,6 @@ const preparePreviewJson = require('./src/preparePreviewJson');
 
 const fs = require('fs').promises;
 
-/**
- * Generate automatically a Json Swagger Document, to provide API documentation.
- *
- * @param   routes A list of arrays where each array is one route of API.
- * 
- * @example ['/endpoint', 'endpoint', 'get', [{
- *  name: '_id',
- *  location: 'path',
- *  required: true,
- *  type: 'string'
- * }], {
- *  name: '',
- *  address: '',
- *  money: 0.00
- * }, true]
- * 
- * [endpoint, category, method, parameters, schema, bearerAuthorization]
- * 
- * Parameters properties: name, location (query, path, header), required (true or false), type: string, number, array ...
- * 
- * @returns Does not have a return but generate a new swagger file under  a choosen folder.
- */
 async function generateSwaggerDoc(routes) {
   const routesFormated = readRoutes(routes);
   const newSwaggerDoc = preparePreviewJson(routesFormated);
