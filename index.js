14f611fdfd16c9b9333e7397db284610a9530b43
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-08-03 03:25:47 -0300

    ref: Remove code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const readRoutes = require('./src/readRoutes');
 const preparePreviewJson = require('./src/preparePreviewJson');
-const contentComparison = require('./src/contentComparison');
 
 const fs = require('fs').promises;
 
@@ -9,15 +8,6 @@ async function generateSwaggerDoc(routes) {
   const newSwaggerDoc = preparePreviewJson(routesFormated);
 
   try {
-    const prevSwagger = await fs.readFile('api-docs/swagger-doc.json', { encoding: 'utf-8', flag: 'r' });
-
-    const compareDocs = contentComparison(prevSwagger, newSwaggerDoc);
-
-    if (compareDocs) {
-      console.log(':: Swagger Document Up-To-Date ::');
-      return;
-    };
-
     await fs.mkdir('api-docs', { recursive: true });
     await fs.writeFile('api-docs/swagger-doc.json', JSON.stringify(newSwaggerDoc), { flag: 'w' });
   } catch (error) {
