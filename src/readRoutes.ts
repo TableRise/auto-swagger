0be55bca37b7067a436243192bf07bb2a972fdb7
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-10-14 21:38:29 -0300

    ref: Update

--- src/readRoutes.ts
+++ src/readRoutes.ts
@@ -2,18 +2,14 @@ import { routeFormatedTypes, routeInstance } from './types/routesTypes';
 
 export default (routes: routeInstance[]): routeFormatedTypes[] => {
   const routesToProcess = routes.filter((route) => !route.hide);
-  const routesFormated = routesToProcess.map((route) => {
-    if (route.hide) return;
-
-    return {
+  const routesFormated = routesToProcess.map((route) => ({
       path: route.path,
       category: route.options.tag,
       method: route.method,
       params: route.parameters,
       schemaRequest: route.schema,
       auth: route.options.authentication,
-    }
-  });
+  }));
 
   return routesFormated;
 };
