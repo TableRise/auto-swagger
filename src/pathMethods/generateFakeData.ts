76fdb5faec25a96098c05c7a168da09b62f1b41f
Adson Gomes Olivera <adsongoliveira2025@gmail.com>
2026-04-02 18:49:57 -0300

    fix: property access

--- src/pathMethods/generateFakeData.ts
+++ src/pathMethods/generateFakeData.ts
@@ -6,12 +6,12 @@ export default (schema: ZodObject): any => {
     const mock = {};
 
     keys.forEach((key) => {
-        if (schema.shape[key] === 'string') mock[key] = faker.string.sample();
-        if (schema.shape[key] === 'number') mock[key] = faker.number.int();
-        if (schema.shape[key] === 'enum') mock[key] = faker.string.sample();
-        if (schema.shape[key] === 'file') mock[key] = {};
-        if (schema.shape[key] === 'uuid') mock[key] = faker.string.uuid();
-        if (schema.shape[key] === 'email') mock[key] = faker.internet.email();
+        if (schema.shape[key].type === 'string') mock[key] = faker.string.sample();
+        if (schema.shape[key].type === 'number') mock[key] = faker.number.int();
+        if (schema.shape[key].type === 'enum') mock[key] = faker.string.sample();
+        if (schema.shape[key].type === 'file') mock[key] = {};
+        if (schema.shape[key].type === 'uuid') mock[key] = faker.string.uuid();
+        if (schema.shape[key].type === 'email') mock[key] = faker.internet.email();
     })
 
     return mock;
